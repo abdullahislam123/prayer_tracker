d03536d066d467021fe8750caf2a047f809c2e00
Abdullah Islam <155878180+abdullahislam123@users.noreply.github.com>
2026-02-19 22:59:13 +0500

    secured key

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDfwxc8hd0_yFcITocRfrwKlqb9Hr-YaDk",
+  apiKey: "[GCP_API_KEY]",
   authDomain: "salah-pro-tracker.firebaseapp.com",
   projectId: "salah-pro-tracker",
   storageBucket: "salah-pro-tracker.firebasestorage.app",
   messagingSenderId: "555420890201",
-  appId: "1:555420890201:web:b0f4b7fb4c2334a138ed48"
+  appId: "1:555420890201:web:b0f4b7fb4c2334a138ed48",
+  measurementId: "G-568947878"
 };
 
 const app = initializeApp(firebaseConfig);
